211b155fe8ba1624453803503399c97fa808109a
zerixmax <zerix.m@gmail.com>
2025-09-03 20:27:09 +0200

    Create task_o1.py

---
+++ task_o1.py
@@ -0,0 +1,21 @@
+# 1. Person details
+first_name = "Zerix"
+last_name = "Maximuuz"
+birth_year = 1976
+birth_country = "Croatia"
+employment_status = "unemployed"
+weight_kg = 75.5
+gender = "male"
+
+# 2. Print each variable on a new line
+print ()
+print(first_name, end= " ;  ")
+print(last_name)
+print(birth_year)
+print(birth_country)
+print(employment_status)
+print(weight_kg)
+print(gender)
+print(gender)
+print(end=' ')
+
