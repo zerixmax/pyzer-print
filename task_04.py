0c531c153cfa60cedbb03a03e597d6d4a843fb5e
zerixmax <zerix.m@gmail.com>
2025-09-05 21:52:40 +0200

    Update task_04.py

--- task_04.py
+++ task_04.py
@@ -1,2 +1,20 @@
-ime = input('Upiši svoje ime: ')
-print('Pozdrav, ' + ime + '!')
+a = input('Upiši broj: ')
+b = input('Upiši drugi broj: ')
+
+a = float(input ('Upiši broj: ') )
+b = float(input('Upiši drugi broj: '))
+
+#ispisati unesene brojeve u formatu:
+#print('Zbroj brojeva a', a, 'i b', b, 'je', a + b)
+#print('Razlika brojeva a', a, 'i b', b, 'je', a - b)
+#print('Umnozak brojeva a', a, 'i b', b, 'je', a * b)
+#print('Kolicnik brojeva a', a, 'i b', b, 'je', a / b)
+#print('Potencija brojeva a', a, 'i b', b, 'je', a ** b)
+#print('Modula brojeva a', a, 'i b', b, 'je', a % b)
+
+print(f'Zbroj brojeva a {a} i b {b} je {a + b}')
+print(f'Razlika brojeva a {a} i b {b} je {a - b}')
+print(f'Umnozak brojeva a {a} i b {b} je {a * b}')
+print(f'Kolicnik brojeva a {a} i b {b} je {a / b}')
+print(f'Potencija brojeva a {a} i b {b} je {a ** b}')
+print(f'Modula brojeva a {a} i b {b} je {a % b}')   
