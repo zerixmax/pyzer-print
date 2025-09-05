4c18c7dfd9ad27c4208652245f1d6de4b0c60b40
zerixmax <zerix.m@gmail.com>
2025-09-05 21:52:36 +0200

    Create strin_05.py

---
+++ strin_05.py
@@ -0,0 +1,8 @@
+a = 192
+b = 168
+c = 0
+d = 184
+
+print(f'Binarni ispis: {bin(a)}, {bin(b)}, {bin(c)}, {bin(d)}')
+print(f'Oktalno ispis: {oct(a)}, {oct(b)}, {oct(c)}, {oct(d)}')
+print(f'Hexadecimal ispis: {hex(a)}, {hex(b)}, {hex(c)}, {hex(d)}')
